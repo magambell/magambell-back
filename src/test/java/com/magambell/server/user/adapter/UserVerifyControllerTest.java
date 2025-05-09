f6efe784325020593d7af04b2c6b9444f1905861
pmh5574 <g_a_m_e_@naver.com>
2025-05-09 16:27:11 +0900

    test: 회원가입시 이메일 전송 테스트, 인증번호 일치 테스트 추가

--- src/test/java/com/magambell/server/user/adapter/UserVerifyControllerTest.java
+++ src/test/java/com/magambell/server/user/adapter/UserVerifyControllerTest.java
@@ -5,7 +5,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.magambell.server.user.adapter.in.web.VerifyEmailAuthCodeRegisterRequest;
 import com.magambell.server.user.adapter.in.web.VerifyEmailDuplicateRegisterRequest;
+import com.magambell.server.user.adapter.in.web.VerifyEmailSendRegisterRequest;
 import com.magambell.server.user.app.port.in.UserVerifyUseCase;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -38,6 +40,38 @@ class UserVerifyControllerTest {
         // when // then
         mockMvc.perform(
                         post("/api/v1/verify/email/register")
+                                .content(objectMapper.writeValueAsString(request))
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @DisplayName("회원가입시 이메일 인증번호 전송시 성공한다.")
+    @Test
+    void emailRegisterSend() throws Exception {
+        // given
+        VerifyEmailSendRegisterRequest request = new VerifyEmailSendRegisterRequest("test@test.com");
+
+        // when // then
+        mockMvc.perform(
+                        post("/api/v1/verify/email/register/send")
+                                .content(objectMapper.writeValueAsString(request))
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @DisplayName("회원가입시 이메일 인증번호가 일치할시 성공한다.")
+    @Test
+    void emailRegisterAuthCodeCheck() throws Exception {
+        // given
+        VerifyEmailAuthCodeRegisterRequest request = new VerifyEmailAuthCodeRegisterRequest("test@test.com", "test");
+
+        // when // then
+        mockMvc.perform(
+                        post("/api/v1/verify/email/register/authCode")
                                 .content(objectMapper.writeValueAsString(request))
                                 .contentType(MediaType.APPLICATION_JSON)
                 )
