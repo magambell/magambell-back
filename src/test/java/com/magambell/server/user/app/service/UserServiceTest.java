ca97d1c7c8e0f221f199b0e6e30ddba6b1f1eb03
pmh5574 <g_a_m_e_@naver.com>
2025-05-16 16:05:46 +0900

    feat: 테스트

--- src/test/java/com/magambell/server/user/app/service/UserServiceTest.java
+++ src/test/java/com/magambell/server/user/app/service/UserServiceTest.java
@@ -21,7 +21,9 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class UserServiceTest {
 
