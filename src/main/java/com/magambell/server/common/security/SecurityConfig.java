4a648d033d4f3438df175a22d1c772675e7e58ee
pmh5574 <g_a_m_e_@naver.com>
2025-06-23 15:52:01 +0900

    fix: 매장 승인 403오류 수정

--- src/main/java/com/magambell/server/common/security/SecurityConfig.java
+++ src/main/java/com/magambell/server/common/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.magambell.server.user.domain.enums.UserRole;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -51,6 +52,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(permitAllWhiteList)
                         .permitAll()
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/admin")
                         .hasRole(UserRole.ADMIN.name())
                         .anyRequest()
