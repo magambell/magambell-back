d78448577b77340c19cc205c5b0cdfadb18fac0d
pmh5574 <g_a_m_e_@naver.com>
2025-06-23 16:01:32 +0900

    fix: 매장 승인 403오류 수정

--- src/main/java/com/magambell/server/common/config/CorsConfig.java
+++ src/main/java/com/magambell/server/common/config/CorsConfig.java
@@ -15,15 +15,17 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("http://localhost:3000", "http://127.0.0.1:3000", "http://127.0.0.1:5500",
-                "http://localhost:5500",
+
+        config.setAllowedOriginPatterns(List.of(
+                "http://localhost:*",
+                "http://127.0.0.1:*",
                 "https://www.mgbell-server.run"
         ));
-        config.setAllowedHeaders(List.of("*"));
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        config.setMaxAge(3600L);
 
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setExposedHeaders(List.of("Authorization", "RefreshToken"));
+        config.setMaxAge(3600L);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
