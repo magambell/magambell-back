48abec269d599e109a8db9ee4dbe1c4e0f3b9027
pmh5574 <g_a_m_e_@naver.com>
2025-06-23 14:13:00 +0900

    fix: 매장 승인 메소드 변경

--- src/main/java/com/magambell/server/common/security/SecurityConfig.java
+++ src/main/java/com/magambell/server/common/security/SecurityConfig.java
@@ -53,6 +53,7 @@ public class SecurityConfig {
                         .permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/v1/store/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/v1/store").hasRole("OWNER")
+                        .requestMatchers(HttpMethod.PATCH, "/api/v1/store/approve").permitAll() // todo 추후 admin으로 변경
                         .requestMatchers("/admin")
                         .hasRole(UserRole.ADMIN.name())
                         .anyRequest()
