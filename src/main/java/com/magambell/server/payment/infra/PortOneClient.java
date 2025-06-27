c71bfbb1e97f5097de6abef8eec3ed1680f1c2dc
pmh5574 <g_a_m_e_@naver.com>
2025-06-27 18:33:11 +0900

    fix: payment

--- src/main/java/com/magambell/server/payment/infra/PortOneClient.java
+++ src/main/java/com/magambell/server/payment/infra/PortOneClient.java
@@ -4,12 +4,14 @@ import com.magambell.server.common.enums.ErrorCode;
 import com.magambell.server.common.exception.NotFoundException;
 import com.magambell.server.payment.app.port.out.PortOnePort;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @RequiredArgsConstructor
 @Component
 public class PortOneClient implements PortOnePort {
@@ -29,9 +31,12 @@ public class PortOneClient implements PortOnePort {
                 .retrieve()
                 .onStatus(
                         HttpStatusCode::isError,
-                        clientResponse -> Mono.error(
-                                new NotFoundException(ErrorCode.PAYMENT_NOT_FOUND)
-                        )
+                        clientResponse -> {
+                            log.warn("PortOne API 에러 발생: {}", clientResponse.statusCode());
+                            return Mono.error(
+                                    new NotFoundException(ErrorCode.PAYMENT_NOT_FOUND)
+                            );
+                        }
                 )
                 .bodyToMono(PortOnePaymentResponse.class)
                 .block();
