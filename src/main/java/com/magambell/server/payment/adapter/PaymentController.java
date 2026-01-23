21fb5a3c0c11145e8f8b46d821767acb2ea857df
ChaMinsu <hong@gil.dong>
2026-01-23 20:40:38 +0900

    revise_payment
    Signed-off-by: ChaMinsu <hong@gil.dong>

--- src/main/java/com/magambell/server/payment/adapter/PaymentController.java
+++ src/main/java/com/magambell/server/payment/adapter/PaymentController.java
@@ -11,12 +11,14 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @Tag(name = "Payment", description = "Payment API")
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/payment")
